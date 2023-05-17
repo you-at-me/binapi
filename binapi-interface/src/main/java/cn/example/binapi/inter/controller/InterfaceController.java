92470989554b664b89e33ae6c82e4323a1d7f5a9
zyshu <zyshu11571@outlook.com>
2023-05-17 13:19:20 +0800

    remote call

---
+++ binapi-interface/src/main/java/cn/example/binapi/inter/controller/InterfaceController.java
@@ -0,0 +1,14 @@
+package cn.example.binapi.inter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Carl
+ * @since 2023-05-17
+ */
+@RestController
+@RequestMapping
+public class InterfaceController {
+
+}
