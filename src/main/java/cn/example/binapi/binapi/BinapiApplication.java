0ecc734e447840fbba49677ba7559dc872f23b9f
zyshu <zyshu11571@outlook.com>
2023-05-17 09:54:57 +0800

    init project

---
+++ src/main/java/cn/example/binapi/binapi/BinapiApplication.java
@@ -0,0 +1,13 @@
+package cn.example.binapi.binapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/example/binapi/binapi/BinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.example.binapi.binapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
