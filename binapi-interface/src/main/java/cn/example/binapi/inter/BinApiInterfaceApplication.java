27efd0af8bcbe272dac415af4c086147e60175ee
zyshu <zyshu11571@outlook.com>
2023-05-17 11:16:46 +0800

    add start files

---
+++ binapi-interface/src/main/java/cn/example/binapi/inter/BinApiInterfaceApplication.java
@@ -0,0 +1,16 @@
+package cn.example.binapi.inter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carl
+ * @since 2023-05-17
+ */
+@SpringBootApplication
+public class BinApiInterfaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BinApiInterfaceApplication.class, args);
+    }
+
+}
