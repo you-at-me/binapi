2d8e6f8c4d97d92ac025b9788f62dd887082fff6
zyshu <zyshu11571@outlook.com>
2023-05-17 13:19:31 +0800

    remote call tests

---
+++ binapi-interface/src/test/java/cn/example/binapi/inter/BinApiInterfaceTests.java
@@ -0,0 +1,16 @@
+package cn.example.binapi.inter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Carl
+ * @since 2023-05-17
+ */
+@SpringBootTest
+public class BinApiInterfaceTests {
+    @Test
+    void contentTest() {
+
+    }
+}
