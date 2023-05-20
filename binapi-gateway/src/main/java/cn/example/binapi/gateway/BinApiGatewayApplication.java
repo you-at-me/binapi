225967de769eeaba13228c4fb1a31fe455d998ec
zyshu <zyshu11571@outlook.com>
2023-05-20 15:17:41 +0800

    init gateway

---
+++ binapi-gateway/src/main/java/cn/example/binapi/gateway/BinApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.example.binapi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carl
+ * @since 2023-05-20
+ */
+@SpringBootApplication
+public class BinApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BinApiGatewayApplication.class, args);
+    }
+}
