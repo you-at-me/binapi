753e3eb15da2ffd83411128d6c83a3169600ffe5
zyshu <zyshu11571@outlook.com>
2023-05-21 10:49:13 +0800

    update sql

--- sql/binapi.sql
+++ sql/binapi.sql
@@ -5,93 +5,85 @@
  Source Server Type    : MySQL
  Source Server Version : 80031
  Source Host           : localhost:3306
- Source Schema         : open_api
+ Source Schema         : binapi
 
  Target Server Type    : MySQL
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 17/05/2023 12:27:41
+ Date: 21/05/2023 10:44:10
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for interface_info
+-- Table structure for tb_interface_info
 -- ----------------------------
-DROP TABLE IF EXISTS `interface_info`;
-CREATE TABLE `interface_info`  (
+DROP TABLE IF EXISTS `tb_interface_info`;
+CREATE TABLE `tb_interface_info`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '名称',
-  `description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '描述',
-  `method` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '请求类型',
+  `description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '描述',
+  `method` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'GET' COMMENT '请求类型',
   `url` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '接口地址',
   `request_params` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '请求参数',
   `request_header` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '请求头',
   `response_header` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '响应头',
-  `price` decimal(10, 2) NOT NULL COMMENT '计费规则(元/条)' default 0.02,
-  `status` int NOT NULL DEFAULT 0 COMMENT '接口状态（0-关闭，1-开启）',
+  `price` decimal(10, 2) NOT NULL DEFAULT 0.01 COMMENT '计费规则(元/条)\n',
+  `status` int NOT NULL DEFAULT 1 COMMENT '接口状态（0-关闭，1-开启）',
+  `left_num` int UNSIGNED NOT NULL DEFAULT 1000 COMMENT '接口剩余能够被调用的次数',
+  `total_num` int UNSIGNED NOT NULL DEFAULT 0 COMMENT '接口已经被调用的总次数',
   `user_id` bigint NOT NULL COMMENT '创建人',
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   `is_delete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除(0-未删, 1-已删)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '接口信息' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '接口信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of interface_info
+-- Records of tb_interface_info
 -- ----------------------------
-
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('许擎宇', '薛聪健', 'www.cary-king.net', '潘博涛', '谭聪健', 0, '石炫明', 9500534531);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('陆弘文', '白志强', 'www.leslee-kuhn.net', '潘懿轩', '马鸿涛', 0, '陈峻熙', 3982575846);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('毛建辉', '罗文', 'www.rosaria-kilback.io', '冯子默', '彭哲瀚', 0, '赵远航', 121776355);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('彭雨泽', '蔡煜祺', 'www.norris-bergstrom.biz', '董思源', '田晓博', 0, '潘擎宇', 740);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('傅志强', '陈梓晨', 'www.jordan-reinger.com', '金志强', '熊锦程', 0, '邓睿渊', 35542559);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('吕黎昕', '孔越彬', 'www.fe-okon.info', '万伟宸', '林昊然', 0, '孟荣轩', 1445);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('夏雪松', '许子骞', 'www.lashawna-legros.co', '蔡昊然', '胡鹏涛', 0, '钟立辉', 34075514);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('严钰轩', '阎志泽', 'www.kay-funk.biz', '莫皓轩', '郭黎昕', 0, '龚天宇', 70956);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('萧嘉懿', '曹熠彤', 'www.margarette-lindgren.biz', '田泽洋', '邓睿渊', 0, '梁志强', 98);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('杜驰', '冯思源', 'www.vashti-auer.org', '黎健柏', '武博文', 0, '李伟宸', 9);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('史金鑫', '蔡鹏涛', 'www.diann-keebler.org', '徐烨霖', '阎建辉', 0, '李烨伟', 125);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('林炫明', '贾旭尧', 'www.dotty-kuvalis.io', '梁雨泽', '龙伟泽', 0, '许智渊', 79998);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('何钰轩', '赖智宸', 'www.andy-adams.net', '崔思淼', '白鸿煊', 0, '邵振家', 7167482751);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('魏志强', '于立诚', 'www.ione-aufderhar.biz', '朱懿轩', '万智渊', 0, '唐昊强', 741098);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('严君浩', '金胤祥', 'www.duane-boyle.org', '雷昊焱', '侯思聪', 0, '郝思', 580514);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('姚皓轩', '金鹏', 'www.lyda-klein.biz', '杜昊强', '邵志泽', 0, '冯鸿涛', 6546);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('廖驰', '沈泽洋', 'www.consuelo-sipes.info', '彭昊然', '邓耀杰', 0, '周彬', 7761037);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('赖智渊', '邓志泽', 'www.emerson-mann.co', '熊明哲', '贺哲瀚', 0, '田鹏', 381422);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('许涛', '陆致远', 'www.vella-ankunding.name', '贾哲瀚', '莫昊焱', 0, '袁越彬', 4218096);
-insert into `interface_info` (`name`, `description`, `url`, `request_header`, `response_header`, `status`, `method`, `user_id`) values ('吕峻熙', '沈鹏飞', 'www.shari-reichel.org', '郭鸿煊', '覃烨霖', 0, '熊黎昕', 493);
-
+INSERT INTO `tb_interface_info` VALUES (1, '许擎宇', '一个人的夜', 'POST', 'www.cary-king.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 9500534531, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (2, '肖鹏飞', '两个人的风', 'POST', 'www.cha.cn', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 53563, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (3, '列友情', '诗词冯旭', 'POST', 'www.bai.org', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 324525, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (4, '岳文青', '青春依旧', 'POST', 'www.king.net', NULL, 'tos', 'Server', 0.01, 1, 1000, 1000, 63452, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (5, '张博涵', '世上只有骗子是真心的', 'POST', 'www.cary.com', NULL, 'tos', 'Server', 0.01, 1, 1000, 1000, 363, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (6, '刘明', '他是真心地在骗你', 'POST', 'www.ming.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 25, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (7, '张华', '一个人的夜', 'POST', 'www.cong.cn', NULL, 'tos', 'Server', 0.01, 1, 1000, 1000, 535, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (8, '东列', '一个人的夜', 'POST', 'www.cary-king.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 2425452, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (9, '白子墨', '一个人的夜', 'POST', 'www.cary-king.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 2523525252, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (10, '徐金枝', '一个人的夜', 'POST', 'www.cary-king.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 57567, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (11, '蔡启浪', '一个人的夜', 'POST', 'www.cary-king.net', NULL, 'tos', 'Server', 0.01, 0, 1000, 1000, 3635366356, '2023-05-18 14:22:25', '2023-05-18 14:22:25', 0);
+INSERT INTO `tb_interface_info` VALUES (12, '那永远的梦想', '我在哪里，我的梦想就在哪里', 'GET', 'http://localhost:9000/interface/main', NULL, '\'Accept-Encoding\': \'gzip, deflate, br\'', '\'Connection\': \'keep-alive\'', 0.01, 1, 1000, 1000, 1378088041316355, '2023-05-19 14:44:56', '2023-05-19 16:06:50', 0);
 
 -- ----------------------------
--- Table structure for soul_soup
+-- Table structure for tb_soul_soup
 -- ----------------------------
-DROP TABLE IF EXISTS `soul_soup`;
-CREATE TABLE `soul_soup`  (
+DROP TABLE IF EXISTS `tb_soul_soup`;
+CREATE TABLE `tb_soul_soup`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `content` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '鸡汤内容',
+  `content` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '鸡汤内容',
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   `is_delete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '心灵鸡汤表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '心灵鸡汤表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of soul_soup
+-- Records of tb_soul_soup
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for tb_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
   `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
   `account` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账号',
   `phone` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
   `avatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png' COMMENT '用户头像',
   `gender` tinyint NULL DEFAULT NULL COMMENT '性别',
   `role` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user / admin',
@@ -103,32 +95,34 @@ CREATE TABLE `user`  (
   `is_delete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除(0-未删, 1-已删)',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uni_userAccount`(`account`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1378088041316353 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1378088041316356 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user
+-- Records of tb_user
 -- ----------------------------
-INSERT INTO `user` VALUES (1378088041316352, NULL, 'admin', NULL, '2022248374@qq.com', 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png', NULL, 'admin', '194344925efb6fdd57eb0384376fa278', 'b569bd6b1ebb1c9f193b5739df444b7b', 'c2de412d6a0414b1686dd602195a2396', '2023-04-23 15:52:58', '2023-04-23 15:53:30', 0);
+INSERT INTO `tb_user` VALUES (1378088041316353, NULL, 'zyshu', NULL, NULL, 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png', NULL, 'user', 'f4c9beb30393bb3d3d1fc103e37f79e3', '21dde9ef2bf4cb0b3fd3376cf72dc936', '580f6cb9a0d42356ccb955a232bf98bd', '2023-05-19 11:33:10', '2023-05-19 11:33:10', 0);
+INSERT INTO `tb_user` VALUES (1378088041316354, NULL, 'Carl', NULL, NULL, 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png', NULL, 'user', 'f4c9beb30393bb3d3d1fc103e37f79e3', '2cbf1d1a5d039c1717945bc255b57a69', 'd485880b9ece2f54cc3f41b0081895a7', '2023-05-19 11:45:40', '2023-05-19 11:45:40', 0);
+INSERT INTO `tb_user` VALUES (1378088041316355, NULL, 'admin', NULL, NULL, 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png', NULL, 'admin', 'f4c9beb30393bb3d3d1fc103e37f79e3', '63074d5d3ffe0fc40d9581b021312047', '5a0db3f8b6b2a5924939608247ecc929', '2023-05-19 14:35:47', '2023-05-19 14:36:03', 0);
 
 -- ----------------------------
--- Table structure for user_interface_info
+-- Table structure for tb_user_interface_info
 -- ----------------------------
-DROP TABLE IF EXISTS `user_interface_info`;
-CREATE TABLE `user_interface_info`  (
+DROP TABLE IF EXISTS `tb_user_interface_info`;
+CREATE TABLE `tb_user_interface_info`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_id` bigint NOT NULL COMMENT '调用用户 id',
   `interface_info_id` bigint NOT NULL COMMENT '接口 id',
-  `total_num` int NOT NULL DEFAULT 0 COMMENT '总调用次数',
-  `left_num` int NOT NULL DEFAULT 0 COMMENT '剩余调用次数',
+  `left_num` int NOT NULL DEFAULT 1000 COMMENT '接口剩余调用次数',
+  `total_num` int NOT NULL DEFAULT 0 COMMENT '接口总调用次数',
   `status` int NOT NULL DEFAULT 1 COMMENT '0-禁用，1-正常',
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   `is_delete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除(0-未删, 1-已删)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户调用接口关系' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户调用接口信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user_interface_info
+-- Records of tb_user_interface_info
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
