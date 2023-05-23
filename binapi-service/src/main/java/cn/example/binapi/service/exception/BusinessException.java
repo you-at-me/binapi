480bd1d1d4387d6043e693b905c5513838759376
zyshu <zyshu11571@outlook.com>
2023-05-23 11:01:53 +0800

    update exception handler

--- binapi-service/src/main/java/cn/example/binapi/service/exception/BusinessException.java
+++ binapi-service/src/main/java/cn/example/binapi/service/exception/BusinessException.java
@@ -1,30 +1,18 @@
 package cn.example.binapi.service.exception;
 
-import cn.example.binapi.service.common.ResponseStatus;
+import cn.example.binapi.common.common.ResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * 自定义异常类
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BusinessException extends RuntimeException {
-
-    private final int code;
-
-    public BusinessException(int code, String message) {
-        super(message);
-        this.code = code;
-    }
-
-    public BusinessException(ResponseStatus errorCode) {
-        super(errorCode.getMessage());
-        this.code = errorCode.getCode();
-    }
-
-    public BusinessException(ResponseStatus errorCode, String message) {
-        super(message);
-        this.code = errorCode.getCode();
-    }
-
-    public int getCode() {
-        return code;
-    }
+    private ResponseStatus responseStatus;
 }
--- binapi-service/src/main/java/cn/example/binapi/service/exception/GlobalExceptionHandler.java
+++ binapi-service/src/main/java/cn/example/binapi/service/exception/GlobalExceptionHandler.java
@@ -1,28 +1,29 @@
 package cn.example.binapi.service.exception;
 
-import cn.example.binapi.service.common.BaseResponse;
-import cn.example.binapi.service.common.ResponseStatus;
-import cn.example.binapi.service.common.ResultUtils;
+import cn.example.binapi.common.common.BaseResponse;
+import cn.example.binapi.common.common.ResponseStatus;
+import cn.example.binapi.common.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 /**
  * 全局异常处理器
  */
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public BaseResponse<?> businessExceptionHandler(BusinessException e) {
-        log.error("businessException: " + e.getMessage(), e); // 这里的 e 打印出了所有相关异常，生产阶段建议注释掉
-        return ResultUtils.error(e.getCode(), e.getMessage());
+        log.error("businessException: " + e.getMessage()); // 这里的 e 打印出了所有相关异常，生产阶段建议注释掉
+        return ResultUtils.error(e.getResponseStatus().getCode(), e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
-        log.error("runtimeException", e);
+        log.error("runtimeException");
         return ResultUtils.error(ResponseStatus.SYSTEM_ERROR, e.getMessage());
     }
 }
