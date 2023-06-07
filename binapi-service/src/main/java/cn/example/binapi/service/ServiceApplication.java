f1707c4bed55c51764dc319fe4b4023ed268c22e
zyshu <zyshu11571@outlook.com>
2023-06-07 15:44:04 +0800

    turn on proxy

--- binapi-service/src/main/java/cn/example/binapi/service/ServiceApplication.java
+++ binapi-service/src/main/java/cn/example/binapi/service/ServiceApplication.java
@@ -3,11 +3,12 @@ package cn.example.binapi.service;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
 @EnableDubbo
-// @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
+@EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
 public class ServiceApplication {
 
     public static void main(String[] args) {
